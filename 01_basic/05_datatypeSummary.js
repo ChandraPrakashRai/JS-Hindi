b098e40fbb23336c05e768a600a81dbc5050602f
Chandra Prakash Rai <145516621+ChandraPrakashRai@users.noreply.github.com>
2025-07-05 07:00:46 +0000

    data type summary

---
+++ 01_basic/05_datatypeSummary.js
@@ -0,0 +1,42 @@
+// primitive  ye call by value  behave krte hai
+// 7 types : String , Number , Boolean , null , undefined , Symbol , BigInt
+
+// symbol me hm bhale hi same value pass kre 
+//  but alag alag symbol ka output alag alag hoga
+const id = Symbol("222")
+const id2 = Symbol("222")
+
+console.log(id===id2); //false
+
+
+
+// Reference type (non- primitive) 
+
+// Array , Objects , Functions
+
+//**********  ARRAY******* */
+let arr = [1,2,3,"shyam"]
+console.log(arr[0]);
+console.log(arr[2]);
+console.log(arr[3]);
+
+// ****** OBJECTS******
+let Obj = {
+    name : "CP" ,
+    age : 22,
+    isStudent : true,
+
+}
+console.log(Obj);
+
+// *********FUNCTION**********/
+  // declare a function
+  // function(){}   // function declared 
+//    assign a variable so that we can use as we want
+
+var myFunction = function(){
+    console.log("hiii");
+
+    
+}
+myFunction();
