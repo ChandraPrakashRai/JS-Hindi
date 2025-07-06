a9dd1c73cb5f3c4fa3502357b36600e6eb6eb589
Chandra Prakash Rai <145516621+ChandraPrakashRai@users.noreply.github.com>
2025-07-06 07:04:50 +0000

    math and num

---
+++ 01_basic/06_number_maths.js
@@ -0,0 +1,26 @@
+const score =  220;
+console.log(score);
+
+const balance = new Number(3005);
+console.log(balance);
+
+console.log(balance.toFixed(2));
+
+// +++++++++++*******Maths********++++++++++++++++
+
+console.log(Math);
+//  abs method hai Math me
+// ye negative value ko positive me change krta  hai
+console.log(Math.abs(-5));
+
+// like abs round() is also a method ye numbers k rounfd off krke value return kr deta hai
+
+console.log(Math.round(5.889)); // 6
+console.log(Math.round(5.005)); // 5
+console.log(Math.sqrt(4.1).toFixed(2)); // 2.02
+
+//  many Math function are available 
+// like Math.min
+// like Math.max
+
+// Math.random ki value 0 se 1 ke bichh me kuch bhi aa skta hai 
