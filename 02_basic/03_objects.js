6a120d82b37bd2053223c653fd8942d24a7271ee
Chandra Prakash Rai <145516621+ChandraPrakashRai@users.noreply.github.com>
2025-07-07 18:19:28 +0000

    basic of object

---
+++ 02_basic/03_objects.js
@@ -0,0 +1,22 @@
+// singleton 
+// constructor method 
+Object.create
+
+// object literals
+
+
+const jsUser = {}  // bn gya object , yh ak object hai   
+
+// objects are based on key value 
+
+const user = {
+    name : "CP",
+    age : 22,
+    location: "Delhi",
+    //  isme hm arrays ko bhi declare kr skte hai|
+    lastLoggedIn:["mon" , "tue"],
+}
+
+// accessing of object 
+console.log(user.name);
+console.log(user["name"]);  // anotyher way to acces the elements
