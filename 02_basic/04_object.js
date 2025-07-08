f110bc18b517805603ff46fa9d35337db721ab55
Chandra Prakash Rai <145516621+ChandraPrakashRai@users.noreply.github.com>
2025-07-08 06:39:40 +0000

    Creating Object

---
+++ 02_basic/04_object.js
@@ -0,0 +1,14 @@
+const stud = {
+  name: "CP",
+  age: 22,
+  marks: {
+    math: 98,
+    science: 100,
+  },
+  isPass: true,
+};
+
+console.log(stud);
+console.log(stud.name);
+console.log(stud.marks.math);
+
---
+++ 02_basic/creating_object.js
@@ -0,0 +1,59 @@
+// **********__________OBJECT bnaane ke 5 trike hote hai ______+++++********
+
+// 1. Object using literals 
+// OBJECT banane ka sbse common and mot used way
+
+const obj={
+    name : "CP",
+    age:23,
+    isMarried : false,
+}
+console.log(obj);
+
+// 2. USING CONSTRUCTOR
+// isme hm new object() keyword ka use krte hai 
+
+let stud = new Object();
+stud.name="CP";
+stud.age=22;
+
+console.log(stud);
+
+// 3. using CONSTRUCTOR FUNCTION 
+//  isme hm phle  ak function declare kr dete hai 
+//  function me kuch parameter pass kr deta hai  
+//  jo hm constructor ke through use krte hai y ause update krte hai
+
+function Student(name , age){
+    this.name = name ;
+    this.age = age;
+}
+
+const std1 = new Student("CP" ,22);
+const std2 = new Student("DP" ,22);
+console.log(std2.name);
+
+// 4. usimg OBJECT CREATE 
+// Object.create()
+const person = {
+  greet: function () {
+    console.log("Hello " + this.name);
+  }
+};
+
+const student = Object.create(person);
+student.name = "CP";
+
+student.greet();  // Hello CP
+
+// 5. using class syntax 
+class Student1 {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+const s1 = new Student1("CP", 22);
+console.log(s1.name); // CP
+
