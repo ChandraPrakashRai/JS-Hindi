3d51c2635a2c7a41ddef07b7ae2a535f0eda68cb
Chandra Prakash Rai <145516621+ChandraPrakashRai@users.noreply.github.com>
2025-07-09 18:37:11 +0000

    basic and important

--- 03_basic/02_scope.js
+++ 03_basic/02_scope.js
@@ -10,8 +10,18 @@ var c = 10
   let  a = 10
    const b = 20
    var c = 30
+   console.log("Inner : a" ,a);
+   console.log("Inner : b" ,b);
+   console.log("Inner : c" ,c);
+   
 }
 console.log(a);
 console.log(b);
 console.log(c);
 
+// ++++++***** Important notes****++++++
+// ✅ let and const are block scoped — block ke bahar unka access nahi hota.
+
+// ⚠️ var is function scoped, not block scoped — isliye uska value overwrite ho jaata hai agar block me likha ho.
+
+// ⚠️ Block {} ke andar var likhne ka koi scope safety nahi hota.
