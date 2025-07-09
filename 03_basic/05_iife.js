a28d003009a40425e36493e997419effe3e6e398
Chandra Prakash Rai <145516621+ChandraPrakashRai@users.noreply.github.com>
2025-07-09 21:10:37 +0000

    iife concept

---
+++ 03_basic/05_iife.js
@@ -0,0 +1,14 @@
+// IIFE ek aisa function expression hota hai jo turant hi execute ho jaata hai jaisi hi define hota hai.
+
+// 🔸 Syntax:
+// js
+// Copy code
+// (function() {
+//   // Code goes here
+// })();
+
+(function() {
+  console.log("Hello CP from IIFE!");
+})();
+
+//  named and unnamed iife concept read kr lena 
